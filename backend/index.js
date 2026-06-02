5a40d29fe68108df77019e6332e8441dfa23ee83
TufailMd <jrtufailmd@gmail.com>
2026-06-02 10:15:54 +0530

    fix allowedOrigins url

--- backend/index.js
+++ backend/index.js
@@ -23,7 +23,7 @@ app.use(express.urlencoded({ extended: true }));
 
 const allowedOrigins = [
   "http://localhost:5173",
-  "https://feelslikehome-one.vercel.app",
+  "https://feels-like-home-one.vercel.app",
 ];
 
 app.use(
